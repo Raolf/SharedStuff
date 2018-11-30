e1091a3b4c4b22a256ef51297b4a31f6fb70bdce
Roalf <sebastianbrandt@live.dk>
2018-11-30 13:53:43 +0100

    for loops shows all data

--- FileReaderTest/Scripts.js
+++ FileReaderTest/Scripts.js
@@ -4,12 +4,26 @@ var textArray = [];
 readText();
 
 function readText() {
-    $.get("Data", function (data) {
+    $.get("Data.txt", function (data) {
         text = data;
 
         textArray = $.csv.toArrays(text);
+        var textForm = "";
 
-        document.getElementById("text").innerText = textArray[0][2];
+
+        for(var i = 0; i<textArray.length; i++){
+
+
+            for(var z = 0; z<textArray[i].length; z++){
+                textForm += textArray[i][z];
+            }
+
+
+            textForm += "\r\n";
+        }
+
+
+        document.getElementById("text").innerText = textForm;
     });
 
 
